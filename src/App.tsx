c8c8180249560816eca7ddae004dae67c35545ca
GB-Navarro <gbnavarro@usp.br>
2022-10-01 19:56:40 -0300

    feat: added the ParticlesBackground component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import GlobalStyles from "./globalStyles";
+import ParticlesBackground from "./components/Particles/Particles";
 function App() {
+	
 	return (
 		<>
 			<GlobalStyles></GlobalStyles>
+			<ParticlesBackground></ParticlesBackground>
 			<div className="App">
 				<header className="App-header"></header>
 			</div>
