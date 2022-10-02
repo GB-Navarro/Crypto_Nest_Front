b883d0ea266a13b06e78c63dc6dcdfb63cb0ce2d
GB-Navarro <gbnavarro@usp.br>
2022-10-01 21:50:39 -0300

    refactor: refactored the app function and added the Home component to it

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
+import Home from "./pages/home/Home";
 import GlobalStyles from "./globalStyles";
-import ParticlesBackground from "./components/Particles/Particles";
+
 function App() {
 	
 	return (
 		<>
 			<GlobalStyles></GlobalStyles>
-			<ParticlesBackground></ParticlesBackground>
-			<div className="App">
-				<header className="App-header"></header>
-			</div>
+			<Home></Home>
 		</>
 	);
 }
