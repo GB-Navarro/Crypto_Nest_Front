6e48bf6cc1438961fbb1a827b2db577f9af5e4e0
GB-Navarro <gbnavarro@usp.br>
2022-10-02 14:24:09 -0300

    feat: added more media queries and changed other css attributes to improve the header responsivity

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -14,27 +14,29 @@ const Container = styled.header`
 	display: flex;
 	justify-content: flex-end;
 	font-family: "Encode Sans", sans-serif;
-	font-weight: 700;
-	font-size: 20px;
+	font-weight: 500;
+	font-size: 18px;
 	color: #ffffff;
-	margin-top: 5%;
+	margin: 30px 0px 30px 0px;
 
-	@media (max-width: 450px) {
-		justify-content: space-evenly
+	@media (max-width: 850px) {
+		justify-content: space-evenly;
 	}
 `;
 
 const Menu = styled.ul`
 	list-style-type: none;
-	width: 80vw;
+	width: 50vw;
 	display: flex;
-	justify-content: space-evenly;
+	justify-content: space-around;
 
-	@media (max-width: 600px) {
-		font-size: 4vw;
+	@media (max-width: 850px) {
+		width: 100vw;
+	}
+	@media (min-width: 450px) and (max-width: 600px) {
+		font-size: 3vw;
 	}
 	@media (max-width: 450px) {
-		width: 100vw;
 		font-size: 4vw;
 	}
 `;
