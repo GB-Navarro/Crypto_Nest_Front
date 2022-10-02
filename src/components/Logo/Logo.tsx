698e03eecc578572a021ccce19733ee0382c5186
GB-Navarro <gbnavarro@usp.br>
2022-10-02 11:05:53 -0300

    feat: added some responsivity to the Logo component

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,16 +1,18 @@
-import { IconBox, TittleBox, Tittle } from "./styles";
+import { Container, IconBox, TittleBox, Tittle } from "./styles";
 
 import IonIcon from "@reacticons/ionicons";
 
 export default function Logo() {
 	return (
 		<>
-			<IconBox>
-				<IonIcon name="logo-bitcoin"></IonIcon>
-			</IconBox>
-			<TittleBox>
-				<Tittle> Crypto Nest </Tittle>
-			</TittleBox>
+			<Container>
+				<IconBox>
+					<IonIcon name="logo-bitcoin"></IonIcon>
+				</IconBox>
+				<TittleBox>
+					<Tittle> Crypto Nest </Tittle>
+				</TittleBox>
+			</Container>
 		</>
 	);
 }
--- src/components/Logo/styles.tsx
+++ src/components/Logo/styles.tsx
@@ -1,18 +1,31 @@
 import styled from "styled-components";
 
+export const Container = styled.section`
+	display: flex;
+`;
 export const IconBox = styled.div`
-	color: #003566;
-	width: 5vh;
-	height: 5vh;
-	font-size: 8vh;
+	span {
+		color: rgb(0, 53, 102);
+		font-size: 12vw;
+	}
+
+	@media (min-width: 600px) {
+		span {
+			font-size: 8vw;
+		}
+	}
 `;
 export const TittleBox = styled.div`
-	height: 8vh;
 	display: flex;
 	align-items: center;
-	padding-left: 25px;
+	padding-left: 10px;
 `;
 export const Tittle = styled.h1`
 	font-weight: 900;
 	color: #ffffff;
+	font-size: 6vw;
+
+	@media (min-width: 600px) {
+		font-size: 5vw;
+	}
 `;
