8515e69837d7936664b694edbabe597ebfc9c1f6
GB-Navarro <gbnavarro@usp.br>
2022-10-02 15:16:36 -0300

    feat: added the onChange attribute

--- src/components/Authentication/InputBar/InputBar.tsx
+++ src/components/Authentication/InputBar/InputBar.tsx
@@ -3,7 +3,7 @@ import { Input } from "./styles";
 export default function InputBar(props) {
 	return (
 		<>
-			<Input type={props.type} id={props.id} placeholder={props.placeholder} maxLength={props.maxLength} required></Input>
+			<Input type={props.type} id={props.id} placeholder={props.placeholder} maxLength={props.maxLength} onChange={props.onChange} required></Input>
 		</>
 	);
 }
