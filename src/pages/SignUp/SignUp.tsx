dd71a49981a2a662d9bbee0633a1697cd2c56f94
GB-Navarro <gbnavarro@usp.br>
2022-10-02 11:07:09 -0300

    feat: created the SignUp page

---
+++ src/pages/SignUp/SignUp.tsx
@@ -0,0 +1,35 @@
+import { Container, LogoBox, Form, IconBox } from "./styles";
+
+import IonIcon from "@reacticons/ionicons";
+import Logo from "../../components/Logo/Logo";
+import Header from "../../components/Header/Header";
+import MenuOption from "../../components/Header/MenuOptions";
+import InputBar from "../../components/Authentication/InputBar/InputBar";
+import SendButton from "../../components/Authentication/SendButton/SendButton";
+
+export default function SignUp() {
+	return (
+		<>
+			<Header>
+				<MenuOption> Home </MenuOption>
+				<MenuOption> About Us </MenuOption>
+			</Header>
+			<Container>
+				<LogoBox>
+					<Logo></Logo>
+				</LogoBox>
+				<Form>
+					<InputBar placeholder="Name" type="text" />
+					<InputBar placeholder="E-mail" type="email" />
+					<InputBar placeholder="Password" type="password" />
+					<InputBar placeholder="Confirm Password" type="password" />
+					<SendButton></SendButton>
+				</Form>
+				<IconBox>
+					<IonIcon name="logo-google"></IonIcon>
+					<IonIcon name="logo-github"></IonIcon>
+				</IconBox>
+			</Container>
+		</>
+	);
+}
---
+++ src/pages/SignUp/styles.tsx
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const Container = styled.main`
+	height: 80vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	font-family: "Encode Sans", sans-serif;
+`;
+export const LogoBox = styled.div`
+	display: flex;
+`;
+export const Form = styled.form`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`;
+export const IconBox = styled.div`
+	width: 40vw;
+	display: flex;
+	justify-content: center;
+	margin-top: 3vh;
+	color: #ffffff;
+	font-size: 35px;
+
+	span {
+		margin: 0px 5px 0px 5px;
+	}
+`;
