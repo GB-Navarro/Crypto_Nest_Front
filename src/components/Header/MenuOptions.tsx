70a58ed7a9de98197aac4166a7a58ae5c5ac2344
GB-Navarro <gbnavarro@usp.br>
2022-10-05 11:57:31 -0300

    feat: added some CSS attributes

--- src/components/Header/MenuOptions.tsx
+++ src/components/Header/MenuOptions.tsx
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 
-const MenuOption = styled.li``;
+const MenuOption = styled.li`
+    text-decoration: none;
+    color: #FFFFFF;
+`;
 
 export default MenuOption;
