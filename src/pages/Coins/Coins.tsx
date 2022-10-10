51984ee33ebe2a08ad38da4024861ca545b65cd2
GB-Navarro <gbnavarro@usp.br>
2022-10-09 21:22:33 -0300

    refactor: removed some unecessary comments

--- src/pages/Coins/Coins.tsx
+++ src/pages/Coins/Coins.tsx
@@ -1,13 +1,6 @@
 import { useState, useEffect } from "react";
 import { Container, Box, Background, LoadingBox } from "./styles";
 
-//colocar todas as infos que vem do back-end
-//colocar um icone para os preços verdes e um icone pros preços vermelhos
-//o nome das criptos ta zoando o espaçamento delas!!
-//formatar melhor os números (criar função pra formatar o marketcap)
-//melhorar a semantica das tags
-//ajustar o overflow-x
-//ajustar a questão do nome do usuário
 import axios from "axios";
 import Loading from "../../components/Loading/Loading";
 import UserHeader from "../../components/UserHeader/UserHeader";
@@ -22,7 +15,7 @@ export default function Coins() {
 	const config = {
 		headers: {
 			Authorization:
-				"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsInVzZXJFbWFpbCI6InRlc3RlMUBnbWFpbC5jb20iLCJpYXQiOjE2NjUzMDY5NDQsImV4cCI6MTY2NTMxMDU0NH0.fQ0LNA7KORSfCgi6-83ggyCcNEFivNRT4Rvon4IUuwU",
+				"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsInVzZXJFbWFpbCI6InRlc3RlMUBnbWFpbC5jb20iLCJpYXQiOjE2NjUzMTIyNjcsImV4cCI6MTY2NTMxNTg2N30.ZE_7_H0l_zMG-lZ8k1H1A9wPLKhUvJ27s_4tELNGBhQ",
 		},
 	};
 
@@ -37,9 +30,9 @@ export default function Coins() {
 	return (
 		<>
 			<UserHeader></UserHeader>
-			{/*<Background>
+			<Background>
 				<ParticlesBackground></ParticlesBackground>
-			</Background> */}
+			</Background>
 			{coinsData === null ? (
 				<Container>
 					<LoadingBox>
