01c42063a4aa75f7d2baf781aa7b887a2a5953cc
GB-Navarro <gbnavarro@usp.br>
2022-10-09 21:03:29 -0300

    feat: created the CoinGeneralInfo component

---
+++ src/components/CoinGeneralInfo/styles.tsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const Main = styled.main`
+	width: 85vw;
+	background-color: rgba(0, 53, 102, 0.25);
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	border-radius: 15px;
+	margin-bottom: 15vh;
+	padding: 5vh 0vh 5vh 0vh;
+	color: rgba(0, 8, 20, 0.95);
+`;
+
+export const Tittle = styled.h1`
+	font-family: "Encode Sans", sans-serif;
+	font-weight: 900;
+	color: #ffc300;
+	padding-bottom: 15vh;
+`;
+
+export const BottomBox = styled.div`
+	width: 90%;
+	display: flex;
+	padding-bottom: 5vh;
+`;
