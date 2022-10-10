9e7b69472e5e3e25bc01cbb1578d9f968ac0fc6f
GB-Navarro <gbnavarro@usp.br>
2022-10-10 08:28:50 -0300

    feat: added some values to the userContext attributes

--- src/pages/User/User.tsx
+++ src/pages/User/User.tsx
@@ -1,14 +1,27 @@
 import { Background, Container } from "./styles";
 import { Link } from "react-router-dom";
+import { useContext } from "react";
 
+import userContext from "../../context/userContext";
 import UserMenu from "../../components/UserMenu/UserMenu";
 import UserHeader from "../../components/UserHeader/UserHeader";
 import ParticlesBackground from "../../components/Particles/Particles";
 
 export default function User() {
+
+	const { setName, setEmail, setToken }: any = useContext(userContext);
+
+	setName(localStorage.getItem(process.env.REACT_APP_LOCALSTORAGE_NAME_KEY));
+	setEmail(
+		localStorage.getItem(process.env.REACT_APP_LOCALSTORAGE_EMAIL_KEY)
+	);
+	setToken(
+		localStorage.getItem(process.env.REACT_APP_LOCALSTORAGE_TOKEN_KEY)
+	);
+
 	return (
 		<>
-			<UserHeader />
+			<UserHeader/>
 			<Background>
 				<ParticlesBackground></ParticlesBackground>
 			</Background>
