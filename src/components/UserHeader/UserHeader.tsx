a22ae91f9fe383562bcc9b64cf37758225ebffcb
GB-Navarro <gbnavarro@usp.br>
2022-10-09 21:20:01 -0300

    feat: updated the UserHeader layout

--- src/components/UserHeader/UserHeader.tsx
+++ src/components/UserHeader/UserHeader.tsx
@@ -1,30 +1,33 @@
 import {
-	Header,
 	Container,
+	Header,
+	UserInfo,
 	ProfileImageBox,
 	ProfileImage,
 	NameBox,
 	Name,
-	SignOut,
+	SignOut
 } from "./styles";
 import { Link } from "react-router-dom";
 
 export default function UserHeader() {
 	return (
 		<>
-			<Header>
-				<Container>
-					<Link to="/user">
-						<ProfileImageBox>
-							<ProfileImage src="https://www.bitmag.com.br/wp-content/uploads/2022/03/monkey-7009603_1920-1536x864.jpg" />
-						</ProfileImageBox>
-					</Link>
-					<NameBox>
-						<Name> Nome do usuário </Name>
-					</NameBox>
-				</Container>
-				<SignOut> Sign Out </SignOut>
-			</Header>
+			<Container>
+				<Header>
+					<UserInfo>
+						<Link to="/user">
+							<ProfileImageBox>
+								<ProfileImage src="https://www.bitmag.com.br/wp-content/uploads/2022/03/monkey-7009603_1920-1536x864.jpg" />
+							</ProfileImageBox>
+						</Link>
+						<NameBox>
+							<Name> Nome do usuário </Name>
+						</NameBox>
+					</UserInfo>
+					<SignOut> Sign Out </SignOut>
+				</Header>
+			</Container>
 		</>
 	);
 }
