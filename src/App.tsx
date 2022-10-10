2f587566dad4a50e1ca744a440220a030a12d319
GB-Navarro <gbnavarro@usp.br>
2022-10-10 08:24:01 -0300

    feat: added the userContext in the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 
 import Home from "./pages/Home/Home";
 import User from "./pages/User/User";
@@ -8,20 +9,29 @@ import GlobalStyles from "./globalStyles";
 import Coins from "./pages/Coins/Coins";
 import Coin from "./pages/Coin/Coin";
 
+import userContext from "./context/userContext";
 function App() {
+	const [name, setName] = useState("");
+	const [email, setEmail] = useState("");
+	const [token, setToken] = useState("");
+
 	return (
 		<>
 			<GlobalStyles></GlobalStyles>
-			<BrowserRouter>
-				<Routes>
-					<Route path="/" element={ <Home /> }></Route>
-					<Route path="/sign-up" element={ <SignUp/> }></Route>
-					<Route path="/sign-in" element={ <SignIn/> }></Route>
-					<Route path="/user" element={ <User/> }></Route>
-					<Route path="/coins" element={ <Coins/> }></Route>
-					<Route path="/coin/:id" element={ <Coin/>}></Route>
-				</Routes>
-			</BrowserRouter>
+			<userContext.Provider
+				value={{ name, setName, email, setEmail, token, setToken }}
+			>
+				<BrowserRouter>
+					<Routes>
+						<Route path="/" element={<Home />}></Route>
+						<Route path="/sign-up" element={<SignUp />}></Route>
+						<Route path="/sign-in" element={<SignIn />}></Route>
+						<Route path="/user" element={<User />}></Route>
+						<Route path="/coins" element={<Coins />}></Route>
+						<Route path="/coin/:id" element={<Coin />}></Route>
+					</Routes>
+				</BrowserRouter>
+			</userContext.Provider>
 		</>
 	);
 }
